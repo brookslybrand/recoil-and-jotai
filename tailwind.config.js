0976cc1578b4a2856765845ff197dd0351ec4ce1
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-09-28 08:28:39 -0500

    Remove boilerplate and setup everything else

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['src/**/*.{js,ts,jsx,tsx}'],
+
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
