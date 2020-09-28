d288b962500d883da741aec6b8f03d90a1a3bbcb
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-09-28 08:42:19 -0500

    Add react-axe

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import * as serviceWorker from './serviceWorker'
 
 import './styles/index.css'
 
+if (process.env.NODE_ENV !== 'production') {
+  var axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
